b552800d73efddb5d976f845dc8bc92a96e05845
Alexandr Murzov <smurz@ya.ru>
2018-03-21 22:32:35 +0300

    Client Library UWP integration code samples.

---
+++ NetSamples/DINAPP.Libs.Client.UWP/AppSettingsInterop.cs
@@ -0,0 +1,37 @@
+﻿using Windows.Foundation.Collections;
+using Windows.Storage;
+
+namespace DINAPP.Libs.Client.UWP
+{
+    public class AppSettingsInterop
+    {
+        private const string UserWalletSettingsKey = "DinappPurchaseDialog_userwalletaddress";
+        private readonly IPropertySet _settings;
+
+        public AppSettingsInterop()
+        {
+            _settings = ApplicationData.Current.LocalSettings.Values;
+        }
+
+        /// <summary>
+        /// Saves user wallet address to application settings to use it in further instances of the dialog.
+        /// </summary>
+        /// <param name="userWalletAddress">User wallet address</param>
+        public void SaveUserWalletAddress(string userWalletAddress)
+        {
+            _settings[UserWalletSettingsKey] = userWalletAddress;
+        }
+
+        /// <summary>
+        /// Returns stored user wallet address
+        /// </summary>
+        /// <returns>User wallet address</returns>
+        public string GetStoredUserWalletKey()
+        {
+            if (_settings.ContainsKey(UserWalletSettingsKey))
+                return _settings[UserWalletSettingsKey] as string;
+
+            return null;
+        }
+    }
+}
---
+++ NetSamples/DINAPP.Libs.Client.UWP/WeiToReadablePriceConverter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace DINAPP.Libs.Client.UWP
+{
+    /// <summary>
+    /// Simplifies prices readability, which are in Wei 
+    /// </summary>
+    internal class WeiToReadablePriceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(value is BigInteger bgInteger)) return "0 Eth";
+
+            var dValue = (double)bgInteger;
+            var digits = Math.Ceiling(Math.Log10(dValue));
+
+            if (digits < 4) return value + " Wei";
+
+            if (digits < 7)
+            {
+                var kwei = dValue / Math.Pow(10, 3);
+                return kwei.ToString("F3") + " Kwei";
+            }
+
+            if (digits < 10)
+            {
+                var mwei = dValue / Math.Pow(10, 6);
+                return mwei.ToString("F3") + " Mwei";
+            }
+
+            if (digits < 13)
+            {
+                var gwei = dValue / Math.Pow(10, 9);
+                return gwei.ToString("F3") + " Gwei";
+            }
+
+            if (digits < 16)
+            {
+                var szabo = dValue / Math.Pow(10, 12);
+                return szabo.ToString("F3") + " Szabo";
+            }
+
+            if (digits < 19)
+            {
+                var finney = dValue / Math.Pow(10, 15);
+                return finney.ToString("F3") + " Finney";
+            }
+
+            if (digits >= 19)
+            {
+                var eth = dValue / Math.Pow(10, 18);
+                return eth.ToString("F3") + " Eth";
+            }
+
+            return value + " Wei";
+        }
+        
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
